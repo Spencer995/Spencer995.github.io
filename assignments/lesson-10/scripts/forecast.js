0cf403ae1288e174a9fc5cdbd59a46bccb8dec62
Spencer995 <nwe18001@byui.edu>
2019-03-17 00:31:50 +0100

    https2

--- assignments/lesson-10/scripts/forecast.js
+++ assignments/lesson-10/scripts/forecast.js
@@ -6,9 +6,9 @@ forcastapi.onload = function() {
      getForcast = JSON.parse(forcastapi.responseText);
     console.log(getForcast);
 
-    document.getElementById('mon').innerHTML = '<img src="http://openweathermap.org/img/w/' + getForcast.list[0].weather[0].icon + '.png" alt="icon"> <br />' + Math.round(parseFloat(getForcast.list[0].main.temp)) + '&deg;F';
-	document.getElementById('tue').innerHTML = '<img src="http://openweathermap.org/img/w/' + getForcast.list[1].weather[0].icon + '.png" alt="icon"> <br />' + Math.round(parseFloat(getForcast.list[1].main.temp)) + '&deg;F';
-	document.getElementById('wed').innerHTML = '<img src="http://openweathermap.org/img/w/' + getForcast.list[2].weather[0].icon + '.png" alt="icon"> <br />' + Math.round(parseFloat(getForcast.list[2].main.temp)) + '&deg;F';
-	document.getElementById('thur').innerHTML = '<img src="http://openweathermap.org/img/w/' + getForcast.list[3].weather[0].icon + '.png" alt="icon"> <br />' + Math.round(parseFloat(getForcast.list[3].main.temp)) + '&deg;F';
-	document.getElementById('fri').innerHTML = '<img src="http://openweathermap.org/img/w/' + getForcast.list[4].weather[0].icon + '.png" alt="icon"> <br />' + Math.round(parseFloat(getForcast.list[4].main.temp)) + '&deg;F';
+    document.getElementById('mon').innerHTML = '<img src="https://openweathermap.org/img/w/' + getForcast.list[0].weather[0].icon + '.png" alt="icon"> <br />' + Math.round(parseFloat(getForcast.list[0].main.temp)) + '&deg;F';
+	document.getElementById('tue').innerHTML = '<img src="https://openweathermap.org/img/w/' + getForcast.list[1].weather[0].icon + '.png" alt="icon"> <br />' + Math.round(parseFloat(getForcast.list[1].main.temp)) + '&deg;F';
+	document.getElementById('wed').innerHTML = '<img src="https://openweathermap.org/img/w/' + getForcast.list[2].weather[0].icon + '.png" alt="icon"> <br />' + Math.round(parseFloat(getForcast.list[2].main.temp)) + '&deg;F';
+	document.getElementById('thur').innerHTML = '<img src="https://openweathermap.org/img/w/' + getForcast.list[3].weather[0].icon + '.png" alt="icon"> <br />' + Math.round(parseFloat(getForcast.list[3].main.temp)) + '&deg;F';
+	document.getElementById('fri').innerHTML = '<img src="https://openweathermap.org/img/w/' + getForcast.list[4].weather[0].icon + '.png" alt="icon"> <br />' + Math.round(parseFloat(getForcast.list[4].main.temp)) + '&deg;F';
 }
