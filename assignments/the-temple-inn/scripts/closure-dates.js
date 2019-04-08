0eba66bbbffe8764718e2f590efb28331f72df81
Spencer995 <nwe18001@byui.edu>
2019-04-08 16:38:12 +0100

    made better

--- assignments/the-temple-inn/scripts/closure-dates.js
+++ assignments/the-temple-inn/scripts/closure-dates.js
@@ -1,6 +1,6 @@
 var section = document.querySelector('#satemple');
                         var request = new XMLHttpRequest();
-                        request.open('GET', 'https://byui-cit230.github.io/weather/data/towndata.json', true);
+                        request.open('GET', 'https://spencer995.github.io/assignments/the-temple-inn/scripts/temples-list.json', true);
                         request.responseType = 'json';
                         request.send();
                         request.onload = function() {
@@ -9,23 +9,24 @@ var section = document.querySelector('#satemple');
                         }
                         function townInfo(jsonItem) {
                            
-                            var town=jsonItem['temples'];
-                            for(var i=0; i < town.length; i++){
-                                if(temples[i].name == 'Johannesburg'){
-                            
+                            var temple=jsonItem['temples'];
+                            for(var i=0; i < temple.length; i++){
+                                if(temple[i].name == 'Johannesburg'){
+                                var article = document.createElement('article')
                                 var heading = document.createElement('h3');
                                 var myPara1 = document.createElement('p');
                                 var eventList = document.createElement('ul');
                             
                                 heading.textContent = 'Johannesburg, South-Africa Temple';
                                 myPara1.textContent = 'Closure Dates:';
-                                var closure = temples[i].closure-dates;
+                                var closure = temple[i].closure-dates;
                                 for(var j=0; j < closure.length; j++) {
                                 var listItem = document.createElement('li');
-                                listItem.textContent = events[j];
+                                listItem.textContent = temples[j];
                                 eventList.appendChild(listItem);
                                 }
-                                
+
+                                article.appendChild(heading);
                                 article.appendChild(myPara1);
                                 article.appendChild(eventList);
                                 section.appendChild(article);
