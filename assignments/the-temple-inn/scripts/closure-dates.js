33761ec4cb16205430eced9b700cc818ab24fe88
Spencer995 <nwe18001@byui.edu>
2019-04-08 16:55:25 +0100

    8/4/2019

--- assignments/the-temple-inn/scripts/closure-dates.js
+++ assignments/the-temple-inn/scripts/closure-dates.js
@@ -12,24 +12,24 @@ var section = document.querySelector('#satemple');
                             var temple=jsonItem['temples'];
                             for(var i=0; i < temple.length; i++){
                                 if(temple[i].name == 'Johannesburg'){
-                                var article = document.createElement('article')
+
                                 var heading = document.createElement('h3');
                                 var myPara1 = document.createElement('p');
                                 var eventList = document.createElement('ul');
                             
                                 heading.textContent = 'Johannesburg, South-Africa Temple';
                                 myPara1.textContent = 'Closure Dates:';
-                                var closure = temple[i].closure-dates;
+                                var closure = temple[i].closure;
                                 for(var j=0; j < closure.length; j++) {
                                 var listItem = document.createElement('li');
-                                listItem.textContent = temples[j];
+                                listItem.textContent = temple[j];
                                 eventList.appendChild(listItem);
                                 }
 
-                                article.appendChild(heading);
-                                article.appendChild(myPara1);
-                                article.appendChild(eventList);
-                                section.appendChild(article);
+                                section.appendChild(heading);
+                                section.appendChild(myPara1);
+                                section.appendChild(eventList);
+                    
                               }  
                         }
                         }
